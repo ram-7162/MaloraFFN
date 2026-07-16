80dcac611565ee5af96ddfa8ef82475855d7b537
ram-7162 <rohitjhajhda@gmail.com>
2026-07-17 01:09:17 +0530

    Refactor updating_layers function for layer range

--- src/Layers.py
+++ src/Layers.py
@@ -1,13 +1,12 @@
 from src.MaloraLayer import MALoRADownProjLayer,DenseLoRADownProjLayer,SymmetricMoEDownProjLayer
 
 
-def updating_layers(model, r1, r2, alpha, n_experts,layer_range,mode="malora") :
-    d_model = model.config.hidden_size        
+
+def updating_layers(model, r1, r2, alpha, n_experts, layer_range, mode="malora"):
+    d_model = model.config.hidden_size
     d_ffn   = model.config.intermediate_size
 
-    start, end = layer_range
-
-    for idx in range(start, end):
+    for idx in layer_range:
         original_mlp = model.model.layers[idx].mlp
         if mode == "symmetric_moe":
             new_mlp = SymmetricMoEDownProjLayer(original_mlp, n_experts, r1, r2, d_model, d_ffn, alpha)
@@ -18,9 +17,33 @@ def updating_layers(model, r1, r2, alpha, n_experts,layer_range,mode="malora") :
         else:
             raise ValueError(f"Unknown mode: {mode}")
 
+        
         new_mlp.to(original_mlp.down_proj.weight.device)
 
         model.model.layers[idx].mlp = new_mlp
+    return model
+
+
+# def updating_layers(model, r1, r2, alpha, n_experts,layer_range,mode="malora") :
+#     d_model = model.config.hidden_size        
+#     d_ffn   = model.config.intermediate_size
+
+#     start, end = layer_range
+
+#     for idx in range(start, end):
+#         original_mlp = model.model.layers[idx].mlp
+#         if mode == "symmetric_moe":
+#             new_mlp = SymmetricMoEDownProjLayer(original_mlp, n_experts, r1, r2, d_model, d_ffn, alpha)
+#         elif mode == "lora":
+#             new_mlp = DenseLoRADownProjLayer(original_mlp, r2, d_model, d_ffn, alpha)
+#         elif mode == "malora":
+#             new_mlp = MALoRADownProjLayer(original_mlp, n_experts, r1, r2, d_model, d_ffn, alpha)
+#         else:
+#             raise ValueError(f"Unknown mode: {mode}")
+
+#         new_mlp.to(original_mlp.down_proj.weight.device)
+
+#         model.model.layers[idx].mlp = new_mlp
 
     return model
 
